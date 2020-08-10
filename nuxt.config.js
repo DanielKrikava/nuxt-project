8e02bbc097af7b92f2e3b22e7bb3a5cd579c5fad
DanielKrikava <daniel.krikava@adbros.com>
2020-08-10 11:05:30 +0200

    db.json changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import EventService from './services/EventService.js'
+
 export default {
   mode: 'universal',
   /*
@@ -58,5 +60,14 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
+  },
+  generate: {
+    routes: () => {
+      return EventService.getEvents().then((response) => {
+        return response.data.map((event) => {
+          return '/event/' + event.id
+        })
+      })
+    }
   }
 }
